fccd86e451ab3b86457936a9561b68d1b6715faf
Kushal Bharadwaj Estari <138388144+Rahulchaudhary792@users.noreply.github.com>
2024-10-12 14:51:18 +0530

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,4 +1,4 @@
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost/leaderboard')
+mongoose.connect('mongodb://0.0.0.0:27017/leaderboard')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
